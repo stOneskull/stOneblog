722a219225c4544ec2f20b60b2fe678ecbc4ef31
Stone Skull <stone@23bay.com>
2023-08-14 16:14:12 +1000

    Update site

--- _module.js
+++ _module.js
@@ -5316,6 +5316,23 @@ function create_fragment$8(ctx) {
 				heading: "Latest",
 				items: [
 					{
+						"date": "2023-08-12",
+						"link": {
+							"url": "/2023-08/harry-krishna",
+							"label": "hurry krisna"
+						},
+						"thumbnail": {
+							"alt": "krisna",
+							"src": "https://ogfrjxqovwgsdzubizan.supabase.co/storage/v1/object/public/images/d6c182e2-29f7-45ec-a001-8b5fa4778f97/1691993013152krisna.jpeg",
+							"url": "https://ogfrjxqovwgsdzubizan.supabase.co/storage/v1/object/public/images/d6c182e2-29f7-45ec-a001-8b5fa4778f97/1691993013152krisna.jpeg",
+							"size": 49
+						},
+						"description": {
+							"html": "<h1 id=\"love\">love</h1>",
+							"markdown": "# love"
+						}
+					},
+					{
 						"date": "2023-08-10",
 						"link": {
 							"url": "/2023-08/laksa",
